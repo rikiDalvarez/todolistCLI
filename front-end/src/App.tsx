068090ac43fc4b386b20a5b9a8e80bd5b3a52755
Ricardo <ric.dalvarez@hotmail.com>
2023-06-17 16:19:30 +0200

    add front end with vite-react-ts

---
+++ front-end/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React + NodeJS TodoList <br></br> storing data in txt files</h1>
+      <div className="app">
+        <div className='todo-input'>
+          <input type="text" name="" id="" />
+        </div>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
